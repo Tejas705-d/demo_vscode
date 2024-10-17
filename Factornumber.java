3b8c4961e88430016741eff89dc1b3b97eb0bd9e
Tejas705-d <dolastejas20@gmail.com>
2024-10-17 21:02:25 +0530

    Add files via upload

---
+++ Factornumber.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+class Factornumber {
+public static void main(String [] args){
+ // Create a Scanner
+     Scannner sc=new Scanner (System.in);
+
+    // user for input
+System.out.println("Enter a number: ");
+int num = sc.nextInt();
+int count=0;
+For (int i=1; i<num, i++;
+{
+if (num%1==0)
+{
+ System.out.println(i+" ");
+}
+}
