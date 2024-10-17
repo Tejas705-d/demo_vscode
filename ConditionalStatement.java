8d06b342c6898580336e1d36a16b513151601776
Tejas705-d <dolastejas20@gmail.com>
2024-10-17 21:26:43 +0530

    Add files via upload

---
+++ ConditionalStatement.java
@@ -0,0 +1,13 @@
+  class ConditionalStatement{
+
+public static void main(String [] args){
+System.out.println("exe starts");
+if (true)
+{
+System.out.println("Hello1");
+System.out.println("Hello2");
+}
+System.out.println("exe ends");
+   }
+}
+
