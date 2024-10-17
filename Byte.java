295bd3352775ded6c94e50287ace94513e72107f
Tejas705-d <dolastejas20@gmail.com>
2024-10-17 20:04:35 +0530

    Add files via upload

---
+++ Byte.java
@@ -0,0 +1,12 @@
+class Byte {
+public static void main(String[] args) {
+		System.out.println("O");
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
+		System.out.println("D");
+		System.out.println("E");
+		System.out.println("F");
+	}
+
+}
